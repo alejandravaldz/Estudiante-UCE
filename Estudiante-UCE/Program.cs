4248cd632954d75b779608d66478193c31c94b1c
Alejandra Valdez <vslejandra1804@gmail.com>
2026-07-18 23:48:19 -0400

    Agregar archivos de proyecto.

---
+++ Estudiante-UCE/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.ComponentModel;
+
+Console.WriteLine("Registro de Calificaciones");
+
+
+Console.Write("Ingrese el nombre del estudiante: ");
+string nombreEstudiante = Console.ReadLine();
+
+List<int> Calificaciones = new List<int>();
+
+
+
+for (int i = 1; i <= 4; i++)
+{
+    Console.Write($"Ingrese la calificación: ");
+    int calificacion = int.Parse(Console.ReadLine());
+    Calificaciones.Add(calificacion);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Estudiante: {nombreEstudiante}");
+Console.WriteLine("Calificaciones:");
+
+
+
+
+
+// Mostrar listado de calificaciones
+
+foreach (int c in Calificaciones)
+{
+    Console.WriteLine($" {c}");
+}
+
+
+
+
+//Calcular y mostrar el promedio
+
+int suma = 0;
+foreach (int c in Calificaciones)
+{
+    suma += c;
+}
+
+
+int promedio = suma / Calificaciones.Count;
+
+Console.WriteLine();
+Console.WriteLine($"El promedio de sus calificaciones es: {promedio}");
+        
+
+
+
+
+
